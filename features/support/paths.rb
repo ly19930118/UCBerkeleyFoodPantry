e8675674063269560f7c071bdc9f4ce2269f1b48
Unzi Park <upark.go@gmail.com>
2015-03-04 14:51:04 -0800

    added paths.rb

---
+++ features/support/paths.rb
@@ -0,0 +1,18 @@
+# TL;DR: YOU SHOULD DELETE THIS FILE
+#
+# This file is used by web_steps.rb, which you should also delete
+#
+# You have been warned
+module NavigationHelpers
+  # Maps a name to a path. Used by the
+  #
+  #   When /^I go to (.+)$/ do |page_name|
+  #
+  # step definition in web_steps.rb
+  #
+  def path_to(page_name)
+
+  end
+end
+
+World(NavigationHelpers)

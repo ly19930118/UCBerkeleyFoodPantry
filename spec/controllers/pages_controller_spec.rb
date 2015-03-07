214f6557fb998eea290b02d03f3e73522c4ab689
Patrick Leng <lgy22106@gmail.com>
2015-03-06 21:20:44 -0800

    added more view tests for rspec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,28 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe PagesController do
+    render_views
+
     describe "GET about" do
         it "renders the about template" do
             get :about
             expect(response).to render_template("about")
+            expect(response.body).to match /About Us/im
         end
     end
     describe "GET events" do
-        it "renders the about template" do
+        it "renders the events template" do
             get :events
             expect(response).to render_template("events")
+            expect(response.body).to match /calendar/im
         end
     end
     describe "GET inventory" do
         it "renders the inventory template" do
             get :inventory
             expect(response).to render_template("inventory")
+            expect(response.body).to match /Inventory Form/im
         end
     end
     describe "GET resources" do
         it "renders the resources template" do
             get :resources
             expect(response).to render_template("resources")
+            expect(response.body).to match /Resources/im
         end
     end
 end

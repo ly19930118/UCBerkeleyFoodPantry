35c014049998fcce2517f1e01858cf5498ac8ac2
saasbook <saasbook@saasbook.(none)>
2015-03-12 21:55:00 -0300

    deploy

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,11 @@ module FoodPantry
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.assets.initialize_on_precompile = false
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
     config.active_record.raise_in_transactional_callbacks = true
   end
 end

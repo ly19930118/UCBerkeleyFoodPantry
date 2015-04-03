4d33d89d1c8c06306281268feca0b940acee2f58
Michelle <linglinglikerice@gmail.com>
2015-04-03 09:47:40 -0700

    updated rspec for admin a bit

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -2,24 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "users/new.html.erb", type: :view do
 
-  describe 'find similar director walkthrough' do
+  describe 'being able to login as an admin or a regular user' do
+
     before :each do
-      @movie=double(Movie, :title => "Pee Wee", :director => "Sherman", :id => "1")
+      @user=double(Movie, :title => "Pee Wee", :director => "Sherman", :id => "1")
       Movie.stub(:find).with("1").and_return(@movie)
     end
 
-    it '#3 should have restful routing for similar movies' do
+    it 'should render the login page correctly' do
       { :post => movie_director_path(1) }.
           should route_to(:controller => "movies", :action => "director", :movie_id => "1")
     end
 
-    it '#1 should call the model method to find similar movies' do
+    it 'should find user in database if user account exists' do
       someMovies = [double(Movie, :title => "Pee Wee Two", :director => "Sherman", :id => "2"), double(Movie, :title => "Pee Wee Three", :director => "Sherman", :id => "3")]
       Movie.should_receive(:same_directors).with('Sherman').and_return(someMovies)
       get :director, :movie_id => "1"
     end
 
-    it '#2 should receive click with controller method and grab id' do
+    it 'should return error if user does not exist in database' do
       Movie.stub(:same_directors).with('Sherman').and_return(@movie)
       get :director, :movie_id => "1"
       response.should render_template('director')
@@ -27,6 +28,29 @@ RSpec.describe "users/new.html.erb", type: :view do
     end
 
   end
+
+  describe 'being able to sign up as admin or user' do
+
+    it 'should render the login page correctly' do
+      { :post => movie_director_path(1) }.
+          should route_to(:controller => "movies", :action => "director", :movie_id => "1")
+    end
+
+    it 'should find user in database if user account exists' do
+      someMovies = [double(Movie, :title => "Pee Wee Two", :director => "Sherman", :id => "2"), double(Movie, :title => "Pee Wee Three", :director => "Sherman", :id => "3")]
+      Movie.should_receive(:same_directors).with('Sherman').and_return(someMovies)
+      get :director, :movie_id => "1"
+    end
+
+    it 'should return error if user does not exist in database' do
+      Movie.stub(:same_directors).with('Sherman').and_return(@movie)
+      get :director, :movie_id => "1"
+      response.should render_template('director')
+      assigns(:movies).should == @movie
+    end
+
+  end
+
 
 
 end

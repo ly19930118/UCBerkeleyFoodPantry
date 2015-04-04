5be0bd4c551963ca763211cc9433e13990c27238
Roy Kim <saasbook@saasbook.(none)>
2015-04-04 00:25:10 -0300

    updating contact info

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,2 @@
 class Contact < ActiveRecord::Base
-
-	after_initialize :default_values
-	def default_values
-		self.email = "mailto:ucberkeleyfoodpantry@gmail.com"
-		self.facebook = "http://facebook.com/ucberkeleyfoodpantry"
-   	 	self.location = "Stiles Hall at 2400 Bancroft Way"
-    	self.hours = "Monday: 12 - 3 pm"
-    end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Contact.create!({:email => 'ucberkeleyfoodpantry@gmail.com', :facebook => 'http://facebook.com/ucberkeleyfoodpantry', :location => 'Stiles Hall at 2400 Bancroft Way', :hours => 'Monday: 12 - 3 pm\n
-	    Tuesday: 1 - 3 pm\n
-	    Thursday: 10 - 11 am, 1 - 3 pm\n
-	    Friday: 10 am - 12 pm'})
+Contact.create!({:email => 'ucberkeleyfoodpantry@gmail.com', :facebook => 'http://facebook.com/ucberkeleyfoodpantry', :location => 'Stiles Hall at 2400 Bancroft Way', :hours => "Monday: 12 - 3 pm\nTuesday: 1 - 3 pm\nThursday: 10 - 11 am, 1 - 3 pm\nFriday: 10 am - 12 pm\nSaturday: CLOSED\nSunday: CLOSED"})

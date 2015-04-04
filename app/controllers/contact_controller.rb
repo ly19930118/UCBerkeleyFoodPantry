e70c4ec01228fe7d09576ad2045f92b002d417f0
Unzi Park <upark.go@gmail.com>
2015-04-03 17:14:54 -0700

    changed initial attributes for contact page

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,22 +1,21 @@
 class ContactController < ApplicationController
-	@@email = "mailto:ucberkeleyfoodpantry@gmail.com"
-	@@facebook = "http://facebook.com/ucberkeleyfoodpantry"
-	@@location = "Stiles Hall at 2400 Bancroft Way"
-	@@hours = "Monday: 12 - 3 pm"
 
 	def index
-		@contact = Contact.new 
-  	end
+		@contact = Contact.create(email: "mailto:ucberkeleyfoodpantry@gmail.com",
+      facebook: "http://facebook.com/ucberkeleyfoodpantry",
+      location: "Stiles Hall at 2400 Bancroft Way",
+      hours: "Monday: 12 - 3pm")
+  end
 
-  	def edit
-  		
-  	end
+  def edit
+    @contact = Contact.create params[:id]
+  end
 
-  	def update
-  		@contact = Contact.find(params[:id])
-  		@contact.update_attributes!(params[:contact])
-  		flash[:notice] = "Contact information was successfully updated"
-  		redirect_to '/contact'
-  	end
+  def update
+  	@contact = Contact.find(params[:id])
+  	@contact.update_attributes!(params[:contact])
+  	flash[:notice] = "Contact information was successfully updated"
+  	redirect_to '/contact'
+  end
 
 end

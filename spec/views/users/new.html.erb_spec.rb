eee82ad5addaed55d6fbebb8487b0f5a24cf03ca
Michelle <linglinglikerice@gmail.com>
2015-04-03 18:35:11 -0700

    meh

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -7,22 +7,14 @@ RSpec.describe "users/new.html.erb", type: :view do
 
   describe 'being able to sign up as admin or user' do
 
-    it 'should render the login page correctly' do
-      { :post => movie_director_path(1) }.
-          should route_to(:controller => "movies", :action => "director", :movie_id => "1")
-    end
 
     it 'should find user in database if user account exists' do
-      someMovies = [double(Movie, :title => "Pee Wee Two", :director => "Sherman", :id => "2"), double(Movie, :title => "Pee Wee Three", :director => "Sherman", :id => "3")]
-      Movie.should_receive(:same_directors).with('Sherman').and_return(someMovies)
-      get :director, :movie_id => "1"
+      user = mock('user1')
+      User.should_receive(:find).with(1).and_return(user)
     end
 
     it 'should return error if user does not exist in database' do
-      Movie.stub(:same_directors).with('Sherman').and_return(@movie)
-      get :director, :movie_id => "1"
-      response.should render_template('director')
-      assigns(:movies).should == @movie
+
     end
 
   end

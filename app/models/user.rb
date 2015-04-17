503647e6b7862f1d3a9786cf531dda4364b9a279
Michelle <linglinglikerice@gmail.com>
2015-04-17 13:52:08 -0700

    fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-	before_save { self.userid = userid.downcase }
 	validates :userid,  presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, length: { minimum: 6 }

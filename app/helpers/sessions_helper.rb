219cd17a7c366a2ea0487f6a2232e5417efea2eb
Michelle <linglinglikerice@gmail.com>
2015-04-17 23:10:52 -0700

    one more time

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,7 @@ module SessionsHelper
     @current_user = User.find_by(id: session[:user_id])
   end
   def current_user
-    @current_user
+    @current_user ||= User.find_by(id: session[:user_id])
   end
   def logged_in?
     !@current_user.nil?

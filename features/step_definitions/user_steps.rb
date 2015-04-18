7653dfc9afadeac28a0f6ac35366c2b041c0d5a2
Unzi Park <upark.go@gmail.com>
2015-04-17 23:06:00 -0700

    fixed cuke tests for updating contact info for admin

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,6 +1,6 @@
 Given /the following users exist/ do |users_table|
   users_table.hashes.each do |user|
-    User.create!(user)
+    User.create!(userid: user['userid'], orders_this_month: user['orders_this_month'], password: user['password'], admin: true)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end

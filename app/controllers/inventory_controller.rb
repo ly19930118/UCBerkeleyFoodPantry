da136a66c0bb99b5560e6432cbabb38f7552155d
Patrick Leng <lgy22106@gmail.com>
2015-04-25 15:55:54 -0700

    asd

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -2,15 +2,17 @@ class InventoryController < ApplicationController
   def index
     #pull data from doc
     #i.val i.id i.name
-    @items = [{:val => "1", :id => "id1", :name => "name1"},{:val => "2", :id => "id2", :name => "name2"}]
+    @items = InventoryItem.all
+    #@items = [{:val => "1", :id => "id1", :name => "name1", :limit => 2}, {:val => "2", :id => "id2", :name => "name2", :limit => 1}]
     #@hours = Contact.first
-    
-    
+
+
 
 
   end
 
   def create
+    #check if order more than twice
 
     # check if valid value
 
@@ -18,6 +20,7 @@ class InventoryController < ApplicationController
     #send email
 
     #redirect to survey
+    redirect_to :index
 
   end
 
